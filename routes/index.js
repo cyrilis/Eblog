2269e8cc3e232b5311c3382092ef0eecb7a33fda
cyrilis <houshoushuai@gmail.com>
2013-10-22 19:10:58 +0800

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+
+/*
+ * Routes File
+ * List all routes here
+ */
+
+module.exports=function(app){
+    app.get("/",function(q,s){
+        s.render("index",{title : "express"})
+    });
+    app.get("/reg",function(q,s){
+        s.render("reg",{title:'Register'});
+    });
+    app.post('/reg',function(q,s){
+
+    });
+    app.get('/login',function(q,s){
+        s.render("login",{title:'Login'})
+    });
+    app.post('/login',function(q,s){
+
+    });
+    app.get('/post',function(q,s){
+        s.render('post',{title:"New Post"})
+    });
+    app.post('/post',function(q,s){
+
+    });
+    app.get('/logout',function(q,s){
+
+    })
+}
